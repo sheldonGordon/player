8c8c76d8f5e5d51656cd9e7fe2edee03d03a7940
chatelain <achatelain62000@gmail.com>
2021-03-05 12:15:44 +0100

    Modification pour lecture de video MKV

--- src/main/java/fr/chatelain/player/controller/FolderController.java
+++ src/main/java/fr/chatelain/player/controller/FolderController.java
@@ -8,21 +8,26 @@ import java.util.stream.Stream;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class FolderController {
     
 	@Value("${directory.videos}")
 	private String directoryVideos;
-	
-    @GetMapping("/folder")
+
+	@Value("${proxy.contextPath}")
+    private String contextPath;
+
+	@RequestMapping(method = RequestMethod.GET, value = "/folder")
 	public String folder(Model model) {
 		List<String> files = Stream.of(new File(directoryVideos).listFiles())
 								.filter(file -> !file.isDirectory())
 								.map(File::getName)
 								.collect(Collectors.toList());
 		model.addAttribute("files", files);
+		model.addAttribute("context", contextPath);
 		return "folder";
 	}
 }
--- src/main/java/fr/chatelain/player/controller/PlayerController.java
+++ src/main/java/fr/chatelain/player/controller/PlayerController.java
@@ -6,11 +6,13 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.Files;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.MimeType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,23 +21,39 @@ import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBo
 @Controller
 public class PlayerController {
     
+	private static final Object MIME_TYPE_WEBM = "video/webm";
+
 	@Value("${directory.videos}")
 	private String directoryVideos;
 
+	@Value("${proxy.contextPath}")
+    private String contextPath;
+
 	@RequestMapping(method = RequestMethod.GET, value = "/player/{video}")
-	public String player(@PathVariable String video, Model model) {
+	public String player(@PathVariable String video, Model model) throws IOException {
+		File videoFile = new File(directoryVideos+video);
+		String mimeType = Files.probeContentType(videoFile.toPath());
+		
 		model.addAttribute("video", video);
+		model.addAttribute("context", contextPath);
+		if(mimeType.contains("mp4") || mimeType.contains("ogg")){
+			model.addAttribute("mimeType", mimeType);
+		}else{
+			model.addAttribute("mimeType", MIME_TYPE_WEBM);
+		}
 		return "player";
 	}
 
 	@RequestMapping(method = RequestMethod.GET, value = "/stream/{name}")
-	public StreamingResponseBody stream(@PathVariable String name) throws FileNotFoundException {
+	public StreamingResponseBody stream(@PathVariable String name) throws IOException {
 		File videoFile = new File(directoryVideos+name);
+		
 		final InputStream videoFileStream = new FileInputStream(videoFile);
 		return (os) -> {
 			readAndWrite(videoFileStream, os);
 		};
 	}
+
 	private void readAndWrite(final InputStream is, OutputStream os) throws IOException {
 		byte[] data = new byte[2048];
 		int read = 0;
